7e48c689a3d51c5d93619842f938f2913348f473
EV3KevinDEV <kevinholding92@gmail.com>
2022-03-22 22:24:54 -0700

    ok

---
+++ src/main/java/frc/robot/commands/autonomous/Trajectory/IntakeOn.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous.Trajectory;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.dreadsubsystem.Drivebase;
+import frc.robot.subsystems.dreadsubsystem.MasterSubsystem;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class IntakeOn extends CommandBase {
+  private final MasterSubsystem m_intake;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+
+   //Can be set in robot container to that degree
+  public IntakeOn(MasterSubsystem intake) {
+    m_intake = intake;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements();
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  
+  }
+
+ 
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_intake.autoIntakeSystemOn();
+   
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+}
